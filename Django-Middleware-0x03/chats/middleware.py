4741bc1b69c490c848159f09c4d85b75d1590455
Aaronkefyalew <159499784+Aaronkefyalew@users.noreply.github.com>
2025-09-29 22:17:23 +0300

    Update middleware.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -5,14 +5,12 @@ class RequestLoggingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-     
         user = request.user if request.user.is_authenticated else "Anonymous"
-        log_entry = f"{datetime.datetime.now()} - User: {user} - Path: {request.path}\n"
+        log_entry = f"{datetime.datetime.now()} - User: {user} - Path: {request.path} - Method: {request.method}\n"
 
         # Write log entry to a file
         with open("requests.log", "a") as log_file:
             log_file.write(log_entry)
 
-        
         response = self.get_response(request)
         return response
